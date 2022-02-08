3299a7ab240fba762060ad6fa94da09f42fc13af
nicholas.kaminski <nkaminsk@ida.org>
2022-02-08 10:15:13 -0500

    fixing bugs and adding gitignore

--- install.sh
+++ install.sh
@@ -1,2 +1,3 @@
-ln -s tmux.conf $HOME/.tmux.conf
-ln -s vim.conf $HOME/.vimrc
+configPath="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 : pwd -P)"
+ln -s $configPath/tmux.conf $HOME/.tmux.conf
+ln -s $configPath/vim.conf $HOME/.vimrc
